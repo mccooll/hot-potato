38bd4869d734d626046e54cd58385c94876f3a2e
mccooll <github@no3.ca>
2020-04-06 16:00:20 -0400

    ugly reminder to only use firefox for now

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -11,12 +11,27 @@ export default {
   }),
   props: ['soundServices'],
   mounted: function() {
-    let promise = this.soundServices.fetchBaseAudio();
-    setTimeout(() => this.hide = false );
-    setTimeout(() => {
-      this.hide = true;
-      setTimeout(() => this.$router.push( {name:"2", params: {readyPromise: promise} }), 1000 );
-    }, 3000);
+    if(!this.badBroswer()) {
+      let promise = this.soundServices.fetchBaseAudio();
+      setTimeout(() => this.hide = false );
+      setTimeout(() => {
+        this.hide = true;
+        setTimeout(() => this.$router.push( {name:"2", params: {readyPromise: promise} }), 1000 );
+      }, 3000);
+    }
+  },
+  methods: {
+    badBroswer() {
+      let x = new Blob();
+      if(!x.arrayBuffer) {
+        window.alert("This browser is not supported. Try firefox.");
+        return true;
+      } else if(!window.InstallTrigger) {
+        window.alert("Only firefox is supported, not chrome nor any other.");
+        return true;
+      }
+      return false;
+    }
   }
 }
 </script>
