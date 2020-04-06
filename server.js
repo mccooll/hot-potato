6627ae97cd7ddd0e3da01522a34990b30a55ffa4
mccooll <github@no3.ca>
2020-04-06 19:08:39 -0400

    this is a botched version of server since it messes up the binary data somehow

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require('express');
+var fs = require('fs');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.post('/output', function(request, respond) {
+	var body = '';
+	var filepath = __dirname + "/dist/input";
+	request.on('data', function(data) {
+        body += data;
+    });
+    request.on('end', function (){
+    	if(fs.existsSync(filepath)) {
+    		fs.unlinkSync(filepath);
+    	}
+        fs.appendFile(filepath, body, function() {
+            respond.end();
+        });
+    });
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
