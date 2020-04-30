07db8a4f5a5a9f565b3c690dad72ac74b0bca713
mccooll <github@no3.ca>
2020-04-30 12:20:34 -0400

    make mic less sensitive and add a delay for speaking so folks are quiet

--- src/components/InputSetupAudio.vue
+++ src/components/InputSetupAudio.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content" :class="{ hide: hideAll }">
     <h3>Get your mic ready.</h3>
-    <h2 :class="{ hide: !listening, emerge: listening }">Say a few words.</h2>
+    <h2 :class="{ hide: !listening && !calibrating || hide, emerge: listening || !hide && calibrating }">{{message}}</h2>
     <h2 :class="{ hide: !heard, emerge: heard }">Great</h2>
   </div>
 </template>
@@ -10,13 +10,25 @@
 export default {
   data: () => ({
     heard: false,
+    calibrating: false,
+    //calibratingMsg: "OK",
     listening: false,
-    hideAll: false
+    listeningMsg: "Say a few words.",
+    hideAll: false,
+    hide: false,
+    message: 'OK'
   }),
   props: ['soundServices'],
   mounted: async function() {
     await this.soundServices.requestMic();
-    this.listening = true;
+    this.calibrating = true;
+    setTimeout(() => {
+      this.hide = true;
+      setTimeout(() => {
+        this.message = this.listeningMsg;
+        this.hide = false;
+      }, 1000)
+    }, 1000)
     await this.soundServices.listen().then(() => this.heard = true );
     setTimeout(() => {
       this.hideAll = true;
