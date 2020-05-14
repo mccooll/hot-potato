0178cc5ff8403ba2b71cc89a0ca2f80cad750db9
mccooll <github@no3.ca>
2020-05-14 19:21:22 -0400

    fix error before warning on ios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+if(!window.AudioContext) window.AudioContext = window.webkitAudioContext;
+
 Vue.config.productionTip = false
 
 new Vue({
